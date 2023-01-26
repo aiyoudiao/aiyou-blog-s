c545f1a9d40f8db6028287ec8b692f89350cd3b6
aiyoudiao <newdiao@163.com>
2023-01-26 19:03:10 +0800

    blog: update nav title

--- docs/.vuepress/config/config.nav.js
+++ docs/.vuepress/config/config.nav.js
@@ -13,65 +13,69 @@
 
 module.exports = [
   {
-    text: '笔记',
+    text: "笔记",
     items: [
-      { text: 'JavaScript', link: '/pages/84633490449/' },
-      { text: 'Vue', link: '/pages/2331001041/' },
-      { text: 'React', link: '/pages/18114480448/' },
-      { text: '低代码', link: '/pages/25236260426/' },
-      { text: '线性系统', link: '/pages/35345230523/' },
+      { text: "JavaScript", link: "/pages/84633490449/" },
+      { text: "Vue", link: "/pages/2331001041/" },
+      { text: "React", link: "/pages/18114480448/" },
+      { text: "低代码", link: "/pages/25236260426/" },
+      { text: "线性系统", link: "/pages/35345230523/" },
     ],
   },
   {
-    text: '算法与设计',
+    text: "算法与设计",
     items: [
-      { text: 'LeetCode', link: '/pages/70741550255/' },
-      { text: '算法', link: '/pages/17845450445/' },
-      { text: '数据结构', link: '/pages/90132170217/' },
-      { text: '设计模式', link: '/pages/50546120212/' },
-      { text: 'Other', link: '/pages/02344550255/' },
+      { text: "LeetCode", link: "/pages/70741550255/" },
+      { text: "算法", link: "/pages/17845450445/" },
+      { text: "数据结构", link: "/pages/90132170217/" },
+      { text: "设计模式", link: "/pages/50546120212/" },
+      { text: "Other", link: "/pages/02344550255/" },
     ],
   },
   {
-    text: '技能',
+    text: "技能",
     items: [
-      { text: 'PMP', link: '/pages/82158160216/' },
-      { text: 'Office', link: '/pages/41858590259/' },
-      { text: '面试', link: '/pages/02359360236/' },
-      { text: 'Bash', link: '/pages/73600130213/' },
-    ],
-  },
-  { 
-    text: '历程', 
-    items: [
-      { text: '流年', link: '/pages/83857320232/' },
-      { text: '片段', link: '/pages/93419130213/' },
-      { text: '读书', link: '/pages/99744220322/' },
+      { text: "PMP", link: "/pages/82158160216/" },
+      { text: "Office", link: "/pages/41858590259/" },
+      { text: "面试", link: "/pages/02359360236/" },
+      { text: "Bash", link: "/pages/73600130213/" },
     ],
   },
   {
-    text: '首页',
+    text: "历程",
     items: [
-      { text: '归档', link: '/archives/' },
-      { text: '分类', link: '/categories/' },
-      { text: '标签', link: '/tags/' },
+      { text: "流年往事", link: "/pages/83857320232/" },
+      { text: "经验片段", link: "/pages/93419130213/" },
+      { text: "读书杂感", link: "/pages/99744220322/" },
     ],
   },
   {
-    text: '其它', 
+    text: "首页",
+    items: [
+      { text: "归档", link: "/archives/" },
+      { text: "分类", link: "/categories/" },
+      { text: "标签", link: "/tags/" },
+    ],
+  },
+  {
+    text: "其它",
     items: [
       {
-        text: '简介', link: '/pages/02657130213/'
+        text: "简介",
+        link: "/pages/02657130213/",
       },
       {
-        text: '收藏', link: '/pages/5390102042/'
+        text: "收藏",
+        link: "/pages/5390102042/",
       },
       {
-        text: '有趣', link: '/pages/32309510451/'
+        text: "有趣",
+        link: "/pages/32309510451/",
       },
       {
-        text: '文档', link: '/pages/23313210521/'
+        text: "文档",
+        link: "/pages/23313210521/",
       },
-    ]
+    ],
   },
-]
+];
